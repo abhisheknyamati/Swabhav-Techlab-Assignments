d5f09739934a266d3693ba954fef1794d650c956
Abhishek Rajshekhar Nyamati <Abhishek.Nyamati@asl.mum.sst>
2024-07-20 18:35:55 +0530

    WAP to accept amount as input. Input amount should not exceed 50000.

---
+++ CurrencyDominationCalculator/Amount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrenctCalculator
+{
+    internal class Amount
+    {
+        private int amount;
+        private const int maxAmount = 50000;
+
+        public int Value
+        {
+            get { return amount; }
+            set
+            {
+                if (value > maxAmount)
+                {
+                    throw new ArgumentException("Amount should not exceed 50000.");
+                }
+                if (value % 100 != 0)
+                {
+                    throw new ArgumentException("Amount should be a multiple of 100.");
+                }
+                amount = value;
+            }
+        }
+
+        public void CalculateDenominations(int amount)
+        {
+            int[] denominations = { 2000, 500, 200, 100 };
+            int remainingAmount = amount;
+
+            Console.WriteLine("Denominations for amount: " + amount);
+            foreach (int denomination in denominations)
+            {
+                int count = remainingAmount / denomination;
+                remainingAmount %= denomination;
+                Console.WriteLine(denomination + ": " + count);
+            }
+        }
+    }
+}
---
+++ CurrencyDominationCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrenctCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Amount amount = new Amount();
+
+            try
+            {
+                Console.WriteLine("Enter the amount (must be multiple of 100 and <= 50000): ");
+                int userInput = int.Parse(Console.ReadLine());
+
+                amount.CalculateDenominations(userInput);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a numeric value.");
+            }
+            
+        }
+    }
+}
