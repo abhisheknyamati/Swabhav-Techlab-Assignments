16ef12223a0af0f39eced5155cb3eb6e01035952
Abhishek Rajshekhar Nyamati <Abhishek.Nyamati@asl.mum.sst>
2024-07-20 19:23:25 +0530

    Create a StudentApp with model class Student . Attributes of student include rollno,name,cpga . The name should be minimum 5 length else name will be empty. The cgpa will be range 1 to 10. There will be method to return the percentage from

---
+++ StudentApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = { 
+                new Student(1, "Abhishek", 5.5),
+                new Student(2, "Nyamati", 7.6), 
+                new Student(3, "Rohan", 8.8)
+            };
+
+            foreach (Student student in students)
+            {
+                student.Percentage(student.CGPA);
+                student.PrintDetails(student);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ StudentApp/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    internal class Student
+    {   public static double CGPA_PERCENTAGE_CONSTANT_VALUE = 9.5;
+        private int rollNumber;
+        private string name;
+        private double cgpa;
+        private double percentage;
+
+        public Student(int rollNumber, string name, double cgpa)
+        {
+            RollNumber = rollNumber;
+            Name = name;
+            CGPA = cgpa;
+        }
+
+        public int RollNumber
+        {
+            get { return rollNumber; }
+            set { rollNumber = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { 
+                if (value.Length >= 5) { 
+                    name = value; } 
+                else { throw new ArgumentException("Minimum 5 characters are required in name!"); }
+            }
+        }
+
+        public double CGPA
+        {
+            get { return cgpa; }
+            set
+            {
+                if (value >= 1 && value <= 10)
+                {
+                    cgpa = value;
+                }
+                else { throw new ArgumentException("CGPA must be between 1 and 10!"); }
+            }
+        }
+
+        public double Percentage(double cgpa)
+        {
+            percentage = cgpa * CGPA_PERCENTAGE_CONSTANT_VALUE;
+            return percentage;
+        }
+
+        public void PrintDetails(Student student)
+        {
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Roll Number : " + RollNumber);
+            Console.WriteLine("CGPA : " + CGPA);
+            Console.WriteLine("Percentage : " + percentage);
+        }
+
+    }
+}
