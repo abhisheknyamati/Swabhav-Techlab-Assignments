df1d18d0dccd6611cad55d8c1e96f16a7c9ef330
Abhishek Rajshekhar Nyamati <Abhishek.Nyamati@asl.mum.sst>
2024-08-10 23:53:56 +0530

    guitarApp

---
+++ GuiterApp/Inventory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiterApp
+{
+    internal class Inventory
+    {
+        public List<Guitar> guitarList = new List<Guitar>();
+
+        public void AddGuitar()
+        {
+            Console.WriteLine("Enter the following details to Add Guitar in list :");
+            Console.WriteLine("Serial Number : ");
+            string serialNumber = Console.ReadLine();
+            Console.WriteLine("Price : ");
+            if (!double.TryParse(Console.ReadLine(), out double price))
+            {
+                Console.WriteLine("Invalid input format");
+                return;
+            } 
+            Console.WriteLine("Builder : ");
+            string builder = Console.ReadLine();
+            Console.WriteLine("Model : ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Type : ");
+            string type = Console.ReadLine();
+            Console.WriteLine("Back Wood : ");
+            string backWood = Console.ReadLine();
+            Console.WriteLine("Top Wood : ");
+            string topWood = Console.ReadLine();    
+            
+            Guitar newGuitar = new Guitar(serialNumber, price, builder, model, type, backWood, topWood);
+            guitarList.Add(newGuitar);
+            Console.WriteLine("New Guitar is successfully added to the list.");
+        }
+
+        public Guitar GetGuitar(string serialNumber)
+        {
+            Guitar foundGuitar = guitarList.Find(guitar => guitar.SerialNumber == serialNumber);
+            if (foundGuitar == null)
+            {
+                Console.WriteLine("ERROR 404 : Guitar not found.");
+                return null;
+            }
+            return foundGuitar;
+        }
+
+        public void DisplayAvailableGuitars()
+        {
+            Console.WriteLine("All available Guitars are : ");
+
+            Console.WriteLine("{0,-15} {1,-10} {2,-15} {3,-10} {4,-15} {5,-15} {6,-15}",
+                "Serial Number", "Price", "Builder", "Model", "Type", "Back Wood", "Top Wood");
+
+            Console.WriteLine(new string('-', 95));
+
+            foreach (Guitar guitar in guitarList)
+            {
+                Console.WriteLine("{0,-15} {1,-10} {2,-15} {3,-10} {4,-15} {5,-15} {6,-15}",
+                    guitar.SerialNumber,
+                    guitar.Price,
+                    guitar.Builder,
+                    guitar.Model,
+                    guitar.Type,
+                    guitar.BackWood,
+                    guitar.TopWood);
+            }
+
+            Console.WriteLine(new string('-', 95));
+        }
+    }
+}
---
+++ GuiterApp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace GuiterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory inventory = new Inventory();
+            Console.WriteLine("Welcome to Guiter Finding Application\n");
+            int userInput;
+            while (true)
+            {
+                Console.WriteLine("Choose from following options, ");
+                Console.WriteLine("1.Add Guitar \n2.Get Guitar \n3.Search Guitar \n4.Display all available guitars \n0.Exit");
+                while (true)
+                {
+                    Console.Write("Enter your choice: ");
+                    string input = Console.ReadLine();
+
+                    if (int.TryParse(input, out userInput) && userInput >= 0 && userInput <= 4)
+                    {
+                        break; 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a number between 0 and 4.");
+                    }
+                }
+                switch (userInput)
+                {
+                    case 0:
+                        Console.WriteLine("Thank You for using our application.");
+                        return;
+                    case 1:
+                        inventory.AddGuitar();
+                        break;
+                    case 4:
+                        inventory.DisplayAvailableGuitars();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input! Enter again...");
+                        continue;
+                }
+            }
+        }
+    }
+}
