342f7a0811f10a79fe5c98836b396ac923b82437
Abhishek Rajshekhar Nyamati <Abhishek.Nyamati@asl.mum.sst>
2024-08-12 09:55:12 +0530

    struct data type

---
+++ StructPOC/College.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructPOC
+{
+    public struct College
+    {
+        public int _id;
+        public string _name;
+        public string _university;
+
+
+        public College(int id, string name, string university)
+        {
+            _id = id;
+            _name = name;
+            _university = university;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Id : " + _id);
+            Console.WriteLine("Name : " + _name);
+            Console.WriteLine("University : " + _university);
+        }
+    }
+}
---
+++ StructPOC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructPOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            College college;
+
+            college._id = 1;
+            college._name = "DMCE";
+            college._university = "MU";
+
+            college.Display();  
+
+            College college1 = new College(2, "AC", "MU");
+            college1.Display();
+        }
+    }
+}
